3bb482d9d0259c00abfb4f57c717c67fd474a740
Nicholas <nickama777@gmail.com>
2017-07-19 18:31:45 -0400

    Appointments mostly working

--- healthnet/cal/models.py
+++ healthnet/cal/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.forms import ModelForm, DateTimeField
-from users.models import Patient, Doctor
+from users.models import Patient, Doctor, Person
+import datetime
 
 class Appointment(models.Model):
     description = models.CharField(max_length=1000)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    time = DateTimeField()
+    time = models.DateTimeField(default=datetime.date.today)
 
 class AppointmentForm(ModelForm):
+    time = DateTimeField()
     class Meta:
         model = Appointment
         fields = '__all__'
--- healthnet/cal/views.py
+++ healthnet/cal/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db import transaction
+from django.contrib import messages
 from .models import AppointmentForm, Appointment
 from users.models import Person
 
@@ -9,31 +10,36 @@ from users.models import Person
 @transaction.atomic
 def show_calendar(request):
     if request.user.person.is_patient:
-        #appointments = Appointment.objects.get()
-        appointments = None
-        print(str(appointments)+'showing patient appointments')
+        appointments = Appointment.objects.filter(patient=request.user.person.id)
     elif request.user.person.is_doctor:
-        #All doctor's appointments
-        appointments = None
+        appointments = Appointment.objects.filter(patient=request.user.person.id)
     elif request.user.person.is_nurse:
         #All appointments for the day at hospital
-        appointments = None
+        appointments = Appointment.objects.all()
     else:
-        appointment = []
+        appointments = None
     return render(request, 'cal/calendar.html', {'appointments':appointments})
 
 @login_required
 @transaction.atomic
 def create_appointment(request):
     if request.method == 'POST':
-        if request.user.person.is_patient:
-            appointment_form = AppointmentForm(initial={patient:request.user})
-        if request.user.person.is_doctor:
-            appointment_form = AppointmentForm(initial={doctor:request.user})
+        appointment_form = AppointmentForm(request.POST)
+        if appointment_form.is_valid():
+            appointment = appointment_form.save()
+            #appointment.time = appointment_form.cleaned_data['time']
+            messages.success(request, "Appointment Created!")
+            return redirect('calendar')
+        else:
+            messages.error(request, 'Please correct the error')
     else:
-        appointment_form = AppointmentForm
+        if request.user.person.is_patient:
+            appointment_form = AppointmentForm(initial={'patient':request.user.person.id})
+        if request.user.person.is_doctor:
+            appointment_form = AppointmentForm(initial={'doctor':request.user.person.id})
 
     return render(request, 'cal/appointments.html', {'appointment_form':appointment_form})
+
 
 @login_required
 @transaction.atomic
