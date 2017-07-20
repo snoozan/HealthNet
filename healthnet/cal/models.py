7cb5a520ccac83904577035cd5e5375d6f349ba8
Nicholas <nickama777@gmail.com>
2017-07-20 13:47:47 -0400

    Merged master into Cal, splitting appointment day and time to implement collision

--- healthnet/cal/models.py
+++ healthnet/cal/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.forms import ModelForm, DateTimeField
+from django.forms import ModelForm, DateTimeField, DateField, TimeField
 from users.models import Patient, Doctor, Person
 import datetime
 
@@ -7,10 +7,11 @@ class Appointment(models.Model):
     description = models.CharField(max_length=1000)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    time = models.DateTimeField(default=datetime.date.today)
-
+    day = models.DateField(default=datetime.date.today)
+    time = models.TimeField(default=datetime.time.now)
 class AppointmentForm(ModelForm):
-    time = DateTimeField()
+    date = DateField()
+    time = TimeField()
     class Meta:
         model = Appointment
         fields = ('description', 'patient', 'doctor','time')
