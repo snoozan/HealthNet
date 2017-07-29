9f44e60499e5f6f274d555533dbcfd98a9aac35e
Nicholas <nickama777@gmail.com>
2017-07-29 12:06:33 -0400

    Working on medical

--- healthnet/med_information/views.py
+++ healthnet/med_information/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def admitted(request):
+    if request.method == 'GET':
+
+    return render(request, 'med_information/result.html', {'result_form':result_form})
