4134a421572fa44013876ef1524f863a63087f3e
Scott <saf5151@rit.edu>
2017-07-29 13:17:08 -0400

    Equals sign in urls

--- healthnet/med_information/urls.py
+++ healthnet/med_information/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-=    url(r'^admitted/$', views.admitted, name='admitted_patients'),
+    url(r'^admitted/$', views.admitted, name='admitted_patients'),
 ]
