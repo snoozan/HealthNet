d63192db419be309efe42f2b3f9ea492c35297c8
srj7750 <srj7750@rit.edu>
2017-07-31 15:54:03 -0400

    updated update and view prescription views and added viewprescriptions template

--- healthnet/med_information/urls.py
+++ healthnet/med_information/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
   url(r'^admitted/$', views.admitted, name='admitted_patients'),
   url(r'^result/create/$', views.createTestResult, name='create_test_result'),
   url(r'^prescription/create/$', views.createPrescription, name='create_prescription'),
-  url(r'^record/create/$', views.createRecord, name='create_record')
+  url(r'^record/create/$', views.createRecord, name='create_record'),
+  url(r'^prescription/update/(?P<prescriptionid>[0-9]+)/$', views.updatePrescription, name='update_prescription'),
+  url(r'^prescription/view/(?P<patientid>[0-9]+)/$', views.viewPrescription, name='view_prescription'),
+  url(r'^prescription/view/$', views.viewPrescription, name='view_prescription')
 ]
