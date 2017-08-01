369537b408fa5ea8561a8c25f819af3ee14f41e6
Susan Lunn <skl1958@g.rit.edu>
2017-08-01 00:07:55 -0400

    stats/models

--- healthnet/stats/models.py
+++ healthnet/stats/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import datetime
+from med_information.models.record import Record
 from users.models import Hospital, Patient
 
 
@@ -14,8 +16,11 @@ class Statistics(models.Model):
     def number_admitted_patients(self):
         return Patient.objects.filter(hospital=self.hospital.id, admitted=True)
 
-    def avg_num_visits_patient(self):
-        pass
+    def avg_num_visits_patient(self, patient_id):
+        records = Record.objects.all()
+        patient_visit = ""
+        for record in records:
+            patient_visit = datetime.timedelta(record.startDate, record.endDate)
 
     def avg_visits_length(self):
         pass
